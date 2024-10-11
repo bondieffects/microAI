37996b0fea13575d551e21db87c679a17bdd7b47
bondieffects <36088638+bondieffects@users.noreply.github.com>
2024-10-11 14:11:51 +1100

    Demonstrated Hampel Filter

--- src/main.cpp
+++ src/main.cpp
@@ -2,34 +2,64 @@
 #include <noise.h>
 #include <statistics.h>
 
+extern uint16_t *outputData;
+
 //Interrupt Service Routine(ISR) handle interupt when complete
 //microcontroller wake up from sleep, main program continue
 ISR(ADC_vect){
 
 }
 
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
-  int n = sizeof(noisyData16) / sizeof(uint16_t);
-  //n /= sizeof(uint16_t);
 
+  uint16_t *inputData = hampelNoise;
+  uint16_t n = sizeof(hampelNoise) / sizeof(uint16_t);
+
+  //n /= sizeof(uint16_t);
+  /*
   Serial.print("n: "); Serial.println(n);
-  float val = mean(noisyData16, n);
+  float val = mean(inputData, n);
   Serial.print("mean: "); Serial.println(val);
 
-  val = variance(noisyData16, n, val);
+  val = variance(inputData, n, val);
   Serial.print("variance: "); Serial.println(val);
 
   val = stddev(val);
   Serial.print("standard deviation: "); Serial.println(val);
 
-  val = median(noisyData16, n);
+  val = median(inputData, n);
   Serial.print("median: "); Serial.println(val);
 
-  val = mode(noisyData16, n);
-  Serial.print("mode: "); Serial.println(val);
+  val = mode(inputData, n);
+  Serial.print("mode: "); Serial.println(val);*/
+
+  // Allocate some memory for the output array
+  outputData = (uint16_t *)malloc(n * sizeof(uint16_t));
+  hampel(inputData, n);
+  Serial.print(">");
+  Serial.print("inputData:"); 
+  Serial.print(inputData[0]);
+  Serial.print(",");
+
+  Serial.print("OutputData:"); 
+  Serial.print(outputData[0]);
+  Serial.print(",");
+  Serial.println();
+  delay(10);
+
+  for (uint16_t i = 1; i<n; i++) {
+    Serial.print(">");
+    Serial.print("inputData:"); Serial.print(inputData[i]); Serial.print(",");
+    Serial.print("OutputData:"); Serial.print(outputData[i]); Serial.print(",");
+    Serial.println();
+    delay(10);
+  }
+  
+
 }
 
 void loop() {
